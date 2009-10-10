7a6609be82984286de9ea37b062a2ce07bec112f
Doug Winter <doug.winter@isotoma.com>
2009-10-10 19:39:18 +0100

    added load test

--- spotify/test/test_session.py
+++ spotify/test/test_session.py
@@ -4,10 +4,11 @@ import threading
 
 from spotify import client
 from spotify import _mockspotify
+from spotify._mockspotify import mock_track, mock_album
 
 client.spotify = _mockspotify
 
-class MockClient(client.Client):
+class BaseMockClient(client.Client):
 
     cache_location = "/foo"
     settings_location = "/foo"
@@ -19,20 +20,31 @@ class MockClient(client.Client):
     def __init__(self):
         self.awoken = threading.Event() # used to block until awoken
 
-    def logged_in(self, session, error):
-        print "MockClient: logged_in"
-        username = session.username()
-        self.found_username = username
-        session.logout()
-        self.disconnect()
-
 class TestSession(unittest.TestCase):
 
     def test_initialisation(self):
+        class MockClient(BaseMockClient):
+            def logged_in(self, session, error):
+                print "MockClient: logged_in"
+                username = session.username()
+                self.found_username = username
+                session.logout()
+                self.disconnect()
+
         c = MockClient()
         c.connect()
         self.assertEqual(c.username, c.found_username)
 
+    def test_load(self):
+        class MockClient(BaseMockClient):
+            def logged_in(self, session, error):
+                print "MockClient: logged_in"
+                track = mock_track("foo", 0, mock_album(), 0, 0, 0, 0, 0, 1)
+                session.load(track)
+                session.logout()
+                self.disconnect()
 
+        c = MockClient()
+        c.connect()
 
 

43f0be26a6de2b354789d94f026c5e6cafe552ea
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-06-08 01:41:37 +0200

    Remove copyright/license notices from source files. An overall license file plus git history ought to be enough.

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -1,4 +1,3 @@
-
 class SpotifyError(Exception):
     pass
 
--- src/mockmodule.c
+++ src/mockmodule.c
@@ -1,25 +1,10 @@
-/* $Id$
- *
- * Copyright 2009 Doug Winter
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
 /*
- * Provides mocking for the entire libspotify library.  All sp_ calls are mocked here to provide
- * something approximate to what libspotify would provide.
+ * Provides mocking for the entire libspotify library.  All sp_ calls are
+ * mocked here to provide something approximate to what libspotify would
+ * provide.
  *
- * Functions for creating mock instances of all spotify objects are also provided.
+ * Functions for creating mock instances of all spotify objects are also
+ * provided.
  *
 */
 #include <Python.h>

1a787449f83c3c201c654874b2c74d6c0cd72e6c
Antoine Pierlot-Garcin <antoine@bokbox.com>
2011-08-05 13:49:22 +0200

    Move sp_link* to libmockspotify + fix link tests.

--- src/mockmodule.c
+++ src/mockmodule.c
@@ -26,12 +26,6 @@
 PyObject *SpotifyError;
 PyObject *SpotifyApiVersion;
 
-/************************** MOCK DATA STRUCTURES **************************/
-
-struct sp_link {
-    char data[1024];
-};
-
 /***************************** MOCK EVENT GENERATION ***************************/
 
 PyObject *
@@ -52,122 +46,6 @@ event_trigger(PyObject *self, PyObject *args)
                                              data)->_playlistcontainer);
     }
     Py_RETURN_NONE;
-}
-
-/********************************* MOCK LINK FUNCTIONS ***********************************/
-
-void
-sp_link_add_ref(sp_link * link)
-{
-}
-
-void
-sp_link_release(sp_link * link)
-{
-}
-
-sp_track *
-sp_link_as_track(sp_link * link)
-{
-    if (strncmp(link->data, "link:track:", strlen("link:track:")))
-        return NULL;
-    sp_track *t = malloc(sizeof(sp_track));
-
-    memset(t, 0, sizeof(sp_track));
-    return mocksp_track_create(link->data + strlen("link:track:"), 0, NULL, NULL, 0,
-                       0, 0, 0, 0, 1);
-    return t;
-}
-
-sp_artist *
-sp_link_as_artist(sp_link * link)
-{
-    if (strncmp(link->data, "link:artist:", strlen("link:artist:")))
-        return NULL;
-    return mocksp_artist_create(link->data + strlen("link:artist:"), 1);
-}
-
-sp_album *
-sp_link_as_album(sp_link * link)
-{
-    if (strncmp(link->data, "link:album:", strlen("link:album:")))
-        return NULL;
-    return mocksp_album_create(link->data + strlen("link:album:"),
-                       mocksp_artist_create("mock", 1), 1901, (byte *) "",
-                       SP_ALBUMTYPE_ALBUM, 1, 1);
-}
-
-sp_link *
-sp_link_create_from_track(sp_track * track, int offset)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:track:%s/%d", track->name, offset);
-    return l;
-}
-
-sp_link *
-sp_link_create_from_album(sp_album * album)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:album:%s", album->name);
-    return l;
-}
-
-sp_link *
-sp_link_create_from_playlist(sp_playlist * playlist)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:playlist:%s", playlist->name);
-    return l;
-}
-
-sp_link *
-sp_link_create_from_artist(sp_artist * artist)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:artist:%s", artist->name);
-    return l;
-}
-
-sp_link *
-sp_link_create_from_search(sp_search * s)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:search:%s", s->query);
-    return l;
-}
-
-sp_link *
-sp_link_create_from_string(const char *link)
-{
-    sp_link *l = malloc(sizeof(sp_link));
-
-    memset(l, 0, sizeof(sp_link));
-    sprintf(l->data, "link:%s", link);
-    return l;
-}
-
-int
-sp_link_as_string(sp_link * link, char *buffer, int buffer_size)
-{
-    strncpy(buffer, link->data, buffer_size);
-    return strlen(link->data);
-}
-
-sp_linktype
-sp_link_type(sp_link * link)
-{
-    return 1;
 }
 
 /**************** MOCKING NEW OBJECTS *******************/
--- tests/test_link.py
+++ tests/test_link.py
@@ -7,35 +7,35 @@ from spotify import SpotifyError
 class TestLink(unittest.TestCase):
 
     def test_from_string(self):
-        s = "from_string_test"
+        s = "spotify:artist:test"
         l = Link.from_string(s)
-        self.assertEqual(str(l), "link:from_string_test")
+        self.assertEqual(str(l), "spotify:artist:test")
 
     def test_from_track(self):
         t = mock_track("foo", 0, mock_album("bar", mock_artist("baz", 1), 0, "",
                                             0, 1, 1), 0, 0, 0, 0, 0, 1)
         l2 = Link.from_track(t, 42)
-        self.assertEqual(str(l2), "link:track:foo/42")
+        self.assertEqual(str(l2), "spotify:track:foo/42")
 
     def test_from_album(self):
         a = mock_album("bar", mock_artist("baz", 1), 0, "", 0, 1, 1)
         l2 = Link.from_album(a)
-        self.assertEqual(str(l2), "link:album:bar")
+        self.assertEqual(str(l2), "spotify:album:bar")
 
     def test_from_artist(self):
         a = mock_artist("artist", 1)
         l = Link.from_artist(a)
-        self.assertEqual(str(l), "link:artist:artist")
+        self.assertEqual(str(l), "spotify:artist:artist")
 
     def test_from_search(self):
         s = mock_search("query")
         l2 = Link.from_search(s)
-        self.assertEqual(str(l2), "link:search:query")
+        self.assertEqual(str(l2), "spotify:search:query")
 
     def test_from_playlist(self):
         p = mock_playlist("foo", [])
         l = Link.from_playlist(p)
-        self.assertEqual(str(l), "link:playlist:foo")
+        self.assertEqual(str(l), "spotify:playlist:foo")
 
     def test_type(self):
         t = mock_track("foo", 0, mock_album("bar", mock_artist("baz", 1), 0, "",
@@ -43,34 +43,25 @@ class TestLink(unittest.TestCase):
         l2 = Link.from_track(t, 42)
         self.assertEqual(l2.type(), Link.LINK_TRACK)
 
+    def test_badlink(self):
+        self.assertRaises(SpotifyError, Link.from_string , "BADLINK");
+
     def test_as_track(self):
-        l = Link.from_string("track:as_track_test")
+        l = Link.from_string("spotify:track:as_track_test")
         t = l.as_track()
         self.assertEqual(str(t), "as_track_test")
 
-    def test_as_track_badlink(self):
-        l = Link.from_string("NOTtrack:as_track_test")
-        self.assertRaises(SpotifyError, l.as_track)
-
     def test_as_album(self):
-        l = Link.from_string("album:as_album_test")
+        l = Link.from_string("spotify:album:as_album_test")
         t = l.as_album()
         self.assertEqual(str(t), "as_album_test")
 
-    def test_as_album_badlink(self):
-        l = Link.from_string("NOTalbum:as_album_test")
-        self.assertRaises(SpotifyError, l.as_album)
-
     def test_as_artist(self):
-        l = Link.from_string("artist:test_as_artist")
+        l = Link.from_string("spotify:artist:test_as_artist")
         a = l.as_artist()
         self.assertEqual(str(a), "test_as_artist")
 
-    def test_as_artist_badlink(self):
-        l = Link.from_string("NOTartist:test_as_artist")
-        self.assertRaises(SpotifyError, l.as_artist)
-
-    def test_str(self):
-        s = "str_test"
+    def test_as_string(self):
+        s = "spotify:track:str_test"
         l = Link.from_string(s)
-        self.assertEqual(str(l), "link:str_test")
+        self.assertEqual(str(l), "spotify:track:str_test")

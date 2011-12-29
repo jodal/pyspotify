302a846db50c44627f1a2e5f1b228e81f1727517
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-12-29 12:57:26 +0100

    Refactor GstreamerAudioController to implement the BaseAudioSink interface

--- spotify/audiosink/gstreamer.py
+++ spotify/audiosink/gstreamer.py
@@ -1,87 +1,67 @@
-import gst
 import gobject
+import gst
+import sys
+
+from spotify.audiosink import BaseAudioSink
+
 gobject.threads_init()
 
-DEFAULT_CAPSET = gst.Caps('''\
-audio/x-raw-int,
-endianness=(int)1234,
-channels=(int)2,
-width=(int)16,
-depth=(int)16,
-signed=(boolean)true,
-rate=(int)44100''')
+CAPS_TEMPLATE = """
+   audio/x-raw-int,
+   endianness=(int)%(endianness)s,
+   channels=(int)%(channels)d,
+   width=(int)16,
+   depth=(int)16,
+   signed=(boolean)true,
+   rate=(int)%(sample_rate)d
+"""
 
-class GstreamerAudioController(object):
-    '''
-    Wrapper around gstreamer so that we can use Gstreamer for audio handling,
-    and thus allowing the pyspotify client to work on OS X.
-    '''
+class GstreamerSink(BaseAudioSink):
+    """
+    Audio sink wrapper for systems with Gstreamer installed, which may include
+    Linux, Mac OS X, and Windows systems.
+    """
 
     def __init__(self):
-        self.pipeline = gst.parse_launch(' ! '.join(['appsrc name="application_src"',
-                                                    'audioconvert',
-                                                    'autoaudiosink']))
-        self.source = self.pipeline.get_by_name('application_src')
-        self.source.set_property('caps', DEFAULT_CAPSET)
+        super(GstreamerSink, self).__init__()
+        if sys.byteorder == 'little':
+            self._endianness = '1234'
+        elif sys.byteorder == 'big':
+            self._endianness = '4321'
+        caps_string = CAPS_TEMPLATE % {
+            'endianness': self._endianness,
+            'sample_rate': 44100,
+            'channels': 2,
+        }
+        caps = gst.caps_from_string(caps_string)
+        self._pipeline = gst.parse_launch(' ! '.join([
+            'appsrc name="application_src"',
+            'audioconvert',
+            'autoaudiosink',
+        ]))
+        self._source = self._pipeline.get_by_name('application_src')
+        self._source.set_property('caps', caps)
 
-    def music_delivery(self,
-                       session,
-                       frames,
-                       frame_size,
-                       num_frames,
-                       sample_type,
-                       sample_rate,
-                       channels):
+    def music_delivery(self, session, frames, frame_size, num_frames,
+            sample_type, sample_rate, channels):
         assert sample_type == 0, u'Expects 16-bit signed integer samples'
-        capabilites = """
-            audio/x-raw-int,
-            endianness=(int)1234,
-            channels=(int)%(channels)d,
-            width=(int)16,
-            depth=(int)16,
-            signed=(boolean)true,
-            rate=(int)%(sample_rate)d
-        """ % {
+        caps_string = CAPS_TEMPLATE % {
+            'endianness': self._endianness,
             'sample_rate': sample_rate,
             'channels': channels,
         }
-        self.emit_data(capabilites, bytes(frames))
+        caps = gst.caps_from_string(caps_string)
+        buffer_ = gst.Buffer(frames)
+        buffer_.set_caps(caps)
+        self._source.set_property('caps', caps)
+        self._source.emit('push-buffer', buffer_)
         return num_frames
 
-    def emit_data(self, capabilities, data):
-        """
-        Call this to deliver raw audio data to be played.
-
-        :param capabilities: a GStreamer capabilities string
-        :type capabilities: string
-        :param data: raw audio data to be played
-        """
-        caps = gst.caps_from_string(capabilities)
-        buffer_ = gst.Buffer(buffer(data))
-        buffer_.set_caps(caps)
-        self.source.set_property('caps', caps)
-        self.source.emit('push-buffer', buffer_)
-
     def start(self):
-        """
-        Notify GStreamer that it should start playback.
-
-        :rtype: :class:`True` if successfull, else :class:`False`
-        """
-        return self.pipeline.set_state(gst.STATE_PLAYING)
+        self._pipeline.set_state(gst.STATE_PLAYING)
 
     def stop(self):
-        """
-        Notify GStreamer that it should stop playback.
-
-        :rtype: :class:`True` if successfull, else :class:`False`
-        """
-        return self.pipeline.set_state(gst.STATE_NULL)
+        self._pipeline.set_state(gst.STATE_NULL)
 
     def pause(self):
-        """
-        Notify GStreamer that it should pause playback.
-
-        :rtype: :class:`True` if successfull, else :class:`False`
-        """
-        return self.pipeline.set_state(gst.STATE_PAUSED)
+        self._pipeline.set_state(gst.STATE_PAUSED)

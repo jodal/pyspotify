7a18e3442c697223b3039c832d397afcfb82645c
Antoine Pierlot-Garcin <antoine@bokbox.com>
2011-12-31 16:26:17 +0100

    tests: update link tests.

--- tests/test_link.py
+++ tests/test_link.py
@@ -1,10 +1,26 @@
 import unittest
-from spotify._mockspotify import Link, mock_artist, mock_track, mock_album, mock_playlist, mock_search
+from spotify._mockspotify import Link, mock_artist, mock_track, mock_album
+from spotify._mockspotify import mock_playlist, mock_search, mock_user
+from spotify._mockspotify import registry_add, registry_clean
 from spotify import SpotifyError
 
-# fake change
-
 class TestLink(unittest.TestCase):
+
+    user = mock_user('user')
+    artist = mock_artist('artist')
+    album = mock_album('album', artist)
+    playlist = mock_playlist('playlist', [], user)
+    search = mock_search('query', [], [], [])
+    track = mock_track('track', [artist], album)
+
+    def setUp(self):
+        registry_add('spotify:artist:test_artist', self.artist)
+        registry_add('spotify:album:test_album', self.album)
+        registry_add('spotify:playlist:test_playlist', self.playlist)
+        registry_add('spotify:track:test_track', self.track)
+
+    def tearDown(self):
+        registry_clean()
 
     def test_from_string(self):
         s = "spotify:artist:test"
@@ -12,54 +28,46 @@ class TestLink(unittest.TestCase):
         self.assertEqual(str(l), "spotify:artist:test")
 
     def test_from_track(self):
-        t = mock_track("foo", 0, mock_album("bar", mock_artist("baz", 1), 0, "",
-                                            0, 1, 1), 0, 0, 0, 0, 0, 1)
-        l2 = Link.from_track(t, 42)
-        self.assertEqual(str(l2), "spotify:track:foo/42")
+        l2 = Link.from_track(self.track, 42 * 1000)
+        self.assertEqual(str(l2), "spotify:track:test_track#00:42")
 
     def test_from_album(self):
-        a = mock_album("bar", mock_artist("baz", 1), 0, "", 0, 1, 1)
-        l2 = Link.from_album(a)
-        self.assertEqual(str(l2), "spotify:album:bar")
+        l2 = Link.from_album(self.album)
+        self.assertEqual(str(l2), "spotify:album:test_album")
 
     def test_from_artist(self):
-        a = mock_artist("artist", 1)
-        l = Link.from_artist(a)
-        self.assertEqual(str(l), "spotify:artist:artist")
+        l = Link.from_artist(self.artist)
+        self.assertEqual(str(l), "spotify:artist:test_artist")
 
     def test_from_search(self):
-        s = mock_search("query")
-        l2 = Link.from_search(s)
+        l2 = Link.from_search(self.search)
         self.assertEqual(str(l2), "spotify:search:query")
 
     def test_from_playlist(self):
-        p = mock_playlist("foo", [])
-        l = Link.from_playlist(p)
-        self.assertEqual(str(l), "spotify:playlist:foo")
+        l = Link.from_playlist(self.playlist)
+        self.assertEqual(str(l), "spotify:playlist:test_playlist")
 
     def test_type(self):
-        t = mock_track("foo", 0, mock_album("bar", mock_artist("baz", 1), 0, "",
-                                            0, 1, 1), 0, 0, 0, 0, 0, 1)
-        l2 = Link.from_track(t, 42)
-        self.assertEqual(l2.type(), Link.LINK_TRACK)
+        l = Link.from_track(self.track, 0)
+        self.assertEqual(l.type(), Link.LINK_TRACK)
 
     def test_badlink(self):
-        self.assertRaises(SpotifyError, Link.from_string , "BADLINK");
+        self.assertRaises(SpotifyError, Link.from_string, "BADLINK");
 
     def test_as_track(self):
-        l = Link.from_string("spotify:track:as_track_test")
+        l = Link.from_string("spotify:track:test_track")
         t = l.as_track()
-        self.assertEqual(str(t), "as_track_test")
+        self.assertEqual(str(t), "track")
 
     def test_as_album(self):
-        l = Link.from_string("spotify:album:as_album_test")
+        l = Link.from_string("spotify:album:test_album")
         t = l.as_album()
-        self.assertEqual(str(t), "as_album_test")
+        self.assertEqual(str(t), "album")
 
     def test_as_artist(self):
-        l = Link.from_string("spotify:artist:test_as_artist")
+        l = Link.from_string("spotify:artist:test_artist")
         a = l.as_artist()
-        self.assertEqual(str(a), "test_as_artist")
+        self.assertEqual(str(a), "artist")
 
     def test_as_string(self):
         s = "spotify:track:str_test"

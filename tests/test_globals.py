da807c6e38a5f9ee97e9dac672dc8d857cf64c06
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-09-08 01:58:58 +0200

    Update api_version test for libspotify 12

--- tests/test_globals.py
+++ tests/test_globals.py
@@ -4,4 +4,4 @@ import spotify
 class TestGlobals(unittest.TestCase):
 
     def test_api_version(self):
-        self.assertEqual(spotify.api_version, 11)
+        self.assertEqual(spotify.api_version, 12)

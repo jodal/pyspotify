210d071668d396600d42fc67bd660618065f64a7
Stefan Hoffmann <stefan991@gmail.com>
2013-01-24 21:45:49 +0100

    AlsaSink: clear audio buffers on stop...
    and audio discontinuity.
    this uses my fork of pyalsaaudio which added PCM.stop to stop playback of
    the device (https://github.com/stifal/pyalsaaudio)
    implemented Fallback:
    if PCM.stop is not available, simply close the device and open a new one

--- spotify/audiosink/alsa.py
+++ spotify/audiosink/alsa.py
@@ -16,6 +16,14 @@ class AlsaSink(BaseAudioSink):
         elif sys.byteorder == 'big':
             self._format = alsaaudio.PCM_FORMAT_S16_BE
 
+    def _stop_device(self):
+        if self._device:
+            if hasattr(self._device, 'stop'):
+                self._device.stop()
+            else:
+                self._device.close()
+                self._device = None
+
     def music_delivery(self, session, frames, frame_size, num_frames,
             sample_type, sample_rate, channels):
         if self._device is None:
@@ -23,8 +31,13 @@ class AlsaSink(BaseAudioSink):
             self._device.setperiodsize(self._periodesize)
             self._device.setformat(self._format)
         if num_frames == 0:
+            # flush audio buffers on seek
+            self._stop_device()
             return 0
         self._call_if_needed(self._device.setrate, sample_rate)
         self._call_if_needed(self._device.setchannels, channels)
         return self._device.write(frames)
 
+    def stop(self):
+        if self._device:
+            self._stop_device()

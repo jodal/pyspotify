180ed01647e03f79bd4c14ff0a73b03b0905061c
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-03-28 10:58:28 +0100

    alsa: Style tweaks

--- spotify/audiosink/alsa.py
+++ spotify/audiosink/alsa.py
@@ -3,6 +3,7 @@ import sys
 
 from spotify.audiosink import BaseAudioSink
 
+
 class AlsaSink(BaseAudioSink):
     """Audio sink wrapper for systems with ALSA, e.g. most Linux systems"""
 
@@ -10,21 +11,21 @@ class AlsaSink(BaseAudioSink):
         super(AlsaSink, self).__init__(**kwargs)
         self._mode = kwargs.get('mode', alsaaudio.PCM_NONBLOCK)
         self._device = None
-        self._periodsize = kwargs.get('periodsize', 8192)
+        self._period_size = kwargs.get('period_size', 8192)
         if sys.byteorder == 'little':
             self._format = alsaaudio.PCM_FORMAT_S16_LE
         elif sys.byteorder == 'big':
             self._format = alsaaudio.PCM_FORMAT_S16_BE
 
-    def music_delivery(self, session, frames, frame_size, num_frames,
+    def music_delivery(
+            self, session, frames, frame_size, num_frames,
             sample_type, sample_rate, channels):
         if self._device is None:
             self._device = alsaaudio.PCM(mode=self._mode)
-            self._device.setperiodsize(self._periodsize)
+            self._device.setperiodsize(self._period_size)
             self._device.setformat(self._format)
         if num_frames == 0:
             return 0
         self._call_if_needed(self._device.setrate, sample_rate)
         self._call_if_needed(self._device.setchannels, channels)
         return self._device.write(frames)
-

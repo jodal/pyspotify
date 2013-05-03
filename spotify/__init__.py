758553edd8da7adbfeb2d291c83442dce77c748c
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-05-04 00:44:16 +0200

    Use a class decorator to add enum values to classes

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -21,16 +21,18 @@ def _to_text(chars):
     return ffi.string(chars).decode('utf-8')
 
 
-def _add_enum(obj, prefix):
-    for attr in dir(lib):
-        if attr.startswith(prefix):
-            setattr(obj, attr.replace(prefix, ''), getattr(lib, attr))
+def enum(prefix):
+    def wrapper(obj):
+        for attr in dir(lib):
+            if attr.startswith(prefix):
+                setattr(obj, attr.replace(prefix, ''), getattr(lib, attr))
+        return obj
+    return wrapper
 
 
+@enum('SP_ERROR_')
 class Error(Exception):
     def __init__(self, error_code):
         self.error_code = error_code
         message = _to_text(lib.sp_error_message(error_code))
         super(Error, self).__init__(message)
-
-_add_enum(Error, 'SP_ERROR_')

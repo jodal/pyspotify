ab500891a44e7034e02889acc5f8ac1d44cb9aad
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-05-04 00:56:46 +0200

    Make Error behavior consistent across Pythons

--- tests/test_error.py
+++ tests/test_error.py
@@ -14,21 +14,17 @@ class ErrorTest(unittest.TestCase):
         error = spotify.Error(1)
         self.assertEqual(error.error_code, 1)
 
-    def test_error_has_error_message(self):
-        error = spotify.Error(0)
-        self.assertEqual(error.message, 'No error')
-        self.assertIsInstance(error.message, six.text_type)
-
-        error = spotify.Error(1)
-        self.assertEqual(error.message, 'Invalid library version')
-
     def test_error_has_useful_repr(self):
         error = spotify.Error(0)
-        self.assertEqual(repr(error), b"Error(u'No error',)")
+        self.assertIn('No error', repr(error))
 
-    def test_error_has_useful_str(self):
+    def test_error_has_useful_string_representation(self):
         error = spotify.Error(0)
-        self.assertEqual(str(error), 'No error')
+        self.assertEqual('%s' % error, 'No error')
+        self.assertIsInstance('%s' % error, six.text_type)
+
+        error = spotify.Error(1)
+        self.assertEqual('%s' % error, 'Invalid library version')
 
     def test_error_has_error_constants(self):
         self.assertEqual(spotify.Error.OK, 0)

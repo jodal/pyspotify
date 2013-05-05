fbdf79bbafd92c278c244c54e07449e05431c5d9
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-05-05 15:14:17 +0200

    Add autotest task

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,22 @@
-from fabric.api import local, task
+from fabric.api import execute, local, settings, task
 
 
 @task
 def preprocess_header():
     local('cpp -nostdinc spotify/api.h > spotify/api.processed.h || true')
+
+
+@task
+def test():
+    local('nosetests')
+
+
+@task
+def autotest():
+    while True:
+        local('clear')
+        with settings(warn_only=True):
+            execute(test)
+        local(
+            'inotifywait -q -e create -e modify -e delete '
+            '--exclude ".*\.(pyc|sw.)" -r spotify/ tests/')

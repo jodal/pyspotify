4fbde3643c9d125546c7e85e7f423654431f93ae
Thomas Adamcik <thomas@adamcik.no>
2013-06-02 23:23:16 +0200

    Switch notify main thread to generic session_callback helper.

--- src/session.c
+++ src/session.c
@@ -667,29 +667,16 @@ message_to_user(sp_session * session, const char *message)
 static void
 notify_main_thread(sp_session * session)
 {
-    PyGILState_STATE gstate;
-    PyObject *res, *method;
-
 #ifdef DEBUG
         fprintf(stderr, "[DEBUG]-session- >> notify_main_thread called\n");
 #endif
+
     if (!session_constructed)
         return;
-    gstate = PyGILState_Ensure();
-    Session *psession =
-        (Session *) PyObject_CallObject((PyObject *)&SessionType, NULL);
-    psession->_session = session;
-    PyObject *client = (PyObject *)sp_session_userdata(session);
 
-    if (client != NULL) {
-        method = PyObject_GetAttrString(client, "notify_main_thread");
-        res = PyObject_CallFunctionObjArgs(method, psession, NULL);
-        if (!res)
-            PyErr_WriteUnraisable(method);
-        Py_XDECREF(res);
-        Py_DECREF(method);
-    }
-    Py_DECREF(psession);
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();
+    session_callback(session, "notify_main_thread");
     PyGILState_Release(gstate);
 }
 

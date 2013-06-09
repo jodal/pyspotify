b1d9fc3f08743bf63e2ca15e4824941d85e84e20
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-06-09 12:49:39 +0200

    album: Add is_loaded property

--- spotify/album.py
+++ spotify/album.py
@@ -16,6 +16,11 @@ class Album(object):
         self.sp_album = ffi.gc(sp_album, lib.sp_album_release)
 
     @property
+    def is_loaded(self):
+        """Whether the album's data is loaded."""
+        return bool(lib.sp_album_is_loaded(self.sp_album))
+
+    @property
     def link(self):
         """A :class:`Link` to the album."""
         from spotify.link import Link
--- tests/test_album.py
+++ tests/test_album.py
@@ -26,6 +26,16 @@ class AlbumTest(unittest.TestCase):
 
         lib_mock.sp_album_release.assert_called_with(sp_album)
 
+    def test_is_loaded(self, lib_mock):
+        lib_mock.sp_album_is_loaded.return_value = 1
+        sp_album = spotify.ffi.new('int *')
+        album = spotify.Album(sp_album)
+
+        result = album.is_loaded
+
+        lib_mock.sp_album_is_loaded.assert_called_once_with(sp_album)
+        self.assertTrue(result)
+
     @mock.patch('spotify.link.Link', spec=spotify.Link)
     def test_link_creates_link_to_album(self, link_mock, lib_mock):
         link_mock.return_value = mock.sentinel.link

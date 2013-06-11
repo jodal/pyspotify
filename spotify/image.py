12c541f6a1cdcf1927798385b56266cfe076cd69
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-06-11 20:30:52 +0200

    image: Add is_loaded and error properties, and load()

--- spotify/image.py
+++ spotify/image.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-from spotify import ffi, lib
-from spotify.utils import IntEnum, make_enum
+from spotify import ErrorType, ffi, lib
+from spotify.utils import IntEnum, load, make_enum
 
 
 __all__ = [
@@ -11,12 +11,38 @@ __all__ = [
 
 
 class Image(object):
-    """A Spotify image."""
+    """A Spotify image.
+
+    You can get images from :meth:`Album.cover`, :meth:`Artist.portrait`, or
+    from :meth:`Link.as_image` when the link is of the image type.
+    """
 
     def __init__(self, sp_image, add_ref=True):
         if add_ref:
             lib.sp_image_add_ref(sp_image)
         self.sp_image = ffi.gc(sp_image, lib.sp_image_release)
+
+    @property
+    def is_loaded(self):
+        """Whether the image's data is loaded."""
+        return bool(lib.sp_image_is_loaded(self.sp_image))
+
+    @property
+    def error(self):
+        """An :class:`ErrorType` associated with the image.
+
+        Check to see if there was problems loading the image.
+        """
+        return ErrorType(lib.sp_image_error(self.sp_image))
+
+    def load(self, timeout=None):
+        """Block until the image's data is loaded.
+
+        :param timeout: seconds before giving up and raising an exception
+        :type timeout: float
+        :returns: self
+        """
+        return load(self, timeout=timeout)
 
     @property
     def link(self):
--- tests/test_image.py
+++ tests/test_image.py
@@ -26,6 +26,36 @@ class ImageTest(unittest.TestCase):
 
         lib_mock.sp_image_release.assert_called_with(sp_image)
 
+    def test_is_loaded(self, lib_mock):
+        lib_mock.sp_image_is_loaded.return_value = 1
+        sp_image = spotify.ffi.new('int *')
+        image = spotify.Image(sp_image)
+
+        result = image.is_loaded
+
+        lib_mock.sp_image_is_loaded.assert_called_once_with(sp_image)
+        self.assertTrue(result)
+
+    def test_error(self, lib_mock):
+        lib_mock.sp_image_error.return_value = int(
+            spotify.ErrorType.IS_LOADING)
+        sp_image = spotify.ffi.new('int *')
+        image = spotify.Image(sp_image)
+
+        result = image.error
+
+        lib_mock.sp_image_error.assert_called_once_with(sp_image)
+        self.assertIs(result, spotify.ErrorType.IS_LOADING)
+
+    @mock.patch('spotify.image.load')
+    def test_load(self, load_mock, lib_mock):
+        sp_image = spotify.ffi.new('int *')
+        image = spotify.Image(sp_image)
+
+        image.load(10)
+
+        load_mock.assert_called_with(image, timeout=10)
+
     @mock.patch('spotify.link.Link', spec=spotify.Link)
     def test_link_creates_link_to_image(self, link_mock, lib_mock):
         link_mock.return_value = mock.sentinel.link

8b3f7946dfdb48c7eff41b84869f8dca87241909
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-06-18 20:29:20 +0200

    examples: Add cover download example

---
+++ examples/cover.py
@@ -0,0 +1,13 @@
+import spotify
+
+# Assuming a spotify_appkey.key in the current dir:
+session = spotify.Session()
+
+# Assuming a previous login with remember_me=True and a proper logout:
+session.relogin()
+
+album = spotify.Album('spotify:album:4m2880jivSbbyEGAKfITCa').load()
+cover = album.cover(spotify.ImageSize.LARGE).load()
+
+open('/tmp/cover.jpg', 'w+').write(cover.data)
+open('/tmp/cover.html', 'w+').write('<img src="%s">' % cover.data_uri)

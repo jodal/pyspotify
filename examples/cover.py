8d9372c845b52e87135331a8dd1d56acad16bed0
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-12-17 00:32:26 +0100

    examples: cover.py needs to wait for the login to complete

--- examples/cover.py
+++ examples/cover.py
@@ -6,6 +6,9 @@ session = spotify.Session()
 # Assuming a previous login with remember_me=True and a proper logout:
 session.relogin()
 
+# TODO Process events until logged in, not just once
+session.process_events()
+
 album = spotify.Album('spotify:album:4m2880jivSbbyEGAKfITCa').load()
 cover = album.cover(spotify.ImageSize.LARGE).load()
 

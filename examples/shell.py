8194d4076358a86e2a04b090b2e3562cda5c92b3
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-02-28 23:39:25 +0100

    examples: Simplify shell example by using EventLoop

--- examples/shell.py
+++ examples/shell.py
@@ -2,16 +2,9 @@ from __future__ import unicode_literals
 
 import cmd
 import logging
-import Queue as queue
 import threading
 
 import spotify
-
-
-spotify_queue = queue.Queue()
-spotify_logged_in = threading.Event()
-spotify_logged_out = threading.Event()
-spotify_logged_out.set()
 
 
 class Commander(cmd.Cmd):
@@ -19,11 +12,30 @@ class Commander(cmd.Cmd):
     doc_header = 'Commands'
     prompt = 'spotify> '
 
-    logger = logging.getLogger('commander')
+    logger = logging.getLogger('shell.commander')
 
-    def __init__(self, spotify_queue):
+    def __init__(self):
         cmd.Cmd.__init__(self)
-        self.spotify_queue = spotify_queue
+
+        self.logged_in = threading.Event()
+        self.logged_out = threading.Event()
+        self.logged_out.set()
+
+        self.session = spotify.Session()
+        self.session.on(spotify.SessionEvent.LOGGED_IN, self.on_logged_in)
+        self.session.on(spotify.SessionEvent.LOGGED_OUT, self.on_logged_out)
+
+        self.event_loop = spotify.EventLoop(self.session)
+        self.event_loop.start()
+
+    def on_logged_in(self, session, error_type):
+        # TODO Handle error situations
+        self.logged_in.set()
+        self.logged_out.clear()
+
+    def on_logged_out(self, session):
+        self.logged_in.clear()
+        self.logged_out.set()
 
     def precmd(self, line):
         if line:
@@ -53,119 +65,40 @@ class Commander(cmd.Cmd):
 
     def do_EOF(self, line):
         "Exit"
-        if spotify_logged_in.is_set():
+        if self.logged_in.is_set():
             print('Logging out...')
-            self.spotify_queue.put(('logout',))
-            spotify_logged_out.wait()
-        print('Exiting...')
-        self.spotify_queue.put(('exit',))
+            self.session.logout()
+            self.logged_out.wait()
+        self.event_loop.stop()
+        print('')
         return True
 
     def do_login(self, line):
         "login <username> <password>"
         username, password = line.split(' ', 1)
-        self.spotify_queue.put(('login', username, password))
-        spotify_logged_in.wait()
+        self.session.login(username, password, remember_me=True)
+        self.logged_in.wait()
 
     def do_relogin(self, line):
         "relogin -- login as the previous logged in user"
-        self.spotify_queue.put(('relogin',))
-        spotify_logged_in.wait()
+        try:
+            self.session.relogin()
+            self.logged_in.wait()
+        except spotify.Error as e:
+            self.logger.error(e)
 
     def do_forget_me(self, line):
         "forget_me -- forget the previous logged in user"
-        self.spotify_queue.put(('forget_me',))
+        self.session.forget_me()
 
     def do_logout(self, line):
         "logout"
-        self.spotify_queue.put(('logout',))
-        spotify_logged_out.wait()
+        self.session.logout()
+        self.logged_out.wait()
 
     def do_whoami(self, line):
         "whoami"
-        self.spotify_queue.put(('whoami',))
-
-    def do_play_uri(self, line):
-        "play <spotify track uri>"
-        self.spotify_queue.put(('play_uri', line))
-
-    def do_search(self, line):
-        "search <query>"
-        self.spotify_queue.put(('search', line))
-
-
-def logged_in(session, error):
-    # TODO Handle error situations
-    spotify_logged_in.set()
-    spotify_logged_out.clear()
-
-
-def logged_out(session):
-    spotify_logged_in.clear()
-    spotify_logged_out.set()
-
-
-def notify_main_thread(session):
-    spotify_queue.put(('notify_main_thread',))
-
-
-class SpotifyLoop(threading.Thread):
-
-    logger = logging.getLogger('loop')
-
-    def __init__(self, spotify_queue):
-        super(SpotifyLoop, self).__init__()
-        self.spotify_queue = spotify_queue
-
-        self.session = spotify.Session()
-
-        self.session.on(spotify.SessionEvent.LOGGED_IN, logged_in)
-        self.session.on(spotify.SessionEvent.LOGGED_OUT, logged_out)
-        self.session.on(
-            spotify.SessionEvent.NOTIFY_MAIN_THREAD, notify_main_thread)
-
-    def run(self):
-        self.logger.debug('Spotify event loop started')
-        stop = None
-        timeout = 1
-        while not stop:
-            try:
-                message = self.spotify_queue.get(timeout=timeout)
-                command, args = message[0], message[1:]
-                self.logger.debug('New command: %s%s', command, args)
-                action = getattr(self, 'do_%s' % command)
-                stop = action(*args)
-            except queue.Empty:
-                self.logger.debug('Timeout reached; processing events')
-                timeout = self.session.process_events() / 1000.0
-                self.logger.debug('Waiting %.3fs for next message', timeout)
-        self.logger.debug('Spotify event loop stopped')
-
-    def do_notify_main_thread(self):
-        self.session.process_events()
-
-    def do_login(self, username, password):
-        self.session.login(username, password, remember_me=True)
-
-    def do_relogin(self):
-        try:
-            self.session.relogin()
-        except spotify.Error as e:
-            self.logger.error(e)
-            spotify_logged_in.set()
-            spotify_logged_in.clear()
-
-    def do_forget_me(self):
-        self.session.forget_me()
-
-    def do_logout(self):
-        self.session.logout()
-
-    def do_exit(self):
-        return True
-
-    def do_whoami(self):
-        if spotify_logged_in.is_set():
+        if self.logged_in.is_set():
             self.logger.info(
                 'I am %s aka %s. You can find me at %s',
                 self.session.user.canonical_name,
@@ -176,12 +109,13 @@ class SpotifyLoop(threading.Thread):
                 'I am not logged in, but I may be %s',
                 self.session.remembered_user)
 
-    def do_play_uri(self, uri):
-        if not spotify_logged_in.is_set():
+    def do_play_uri(self, line):
+        "play <spotify track uri>"
+        if not self.logged_in.is_set():
             self.logger.warning('You must be logged in to play')
             return
         try:
-            track = spotify.Link(uri).as_track()
+            track = spotify.Link(line).as_track()
             track.load()
         except (ValueError, spotify.Error) as e:
             self.logger.warning(e)
@@ -190,9 +124,11 @@ class SpotifyLoop(threading.Thread):
         self.session.player.load(track)
         self.logger.info('Playing track')
         self.session.player.play()
+        # TODO Play received audio samples
 
     def do_search(self, query):
-        if not spotify_logged_in.is_set():
+        "search <query>"
+        if not self.logged_in.is_set():
             self.logger.warning('You must be logged in to search')
             return
         try:
@@ -213,10 +149,4 @@ class SpotifyLoop(threading.Thread):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-
-    spotify_loop = SpotifyLoop(spotify_queue)
-    spotify_loop.start()
-
-    Commander(spotify_queue).cmdloop()
-
-    spotify_loop.join()
+    Commander().cmdloop()

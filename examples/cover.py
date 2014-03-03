612c7c761d4bfa35742d51e9ad2283a347d75045
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-03-03 23:12:25 +0100

    examples: Update cover.py, fix Py3 compat

--- examples/cover.py
+++ examples/cover.py
@@ -6,11 +6,11 @@ session = spotify.Session()
 # Assuming a previous login with remember_me=True and a proper logout:
 session.relogin()
 
-# TODO Process events until logged in, not just once
-session.process_events()
+while session.connection_state != spotify.ConnectionState.LOGGED_IN:
+    session.process_events()
 
-album = spotify.Album('spotify:album:4m2880jivSbbyEGAKfITCa').load()
+album = session.get_album('spotify:album:4m2880jivSbbyEGAKfITCa').load()
 cover = album.cover(spotify.ImageSize.LARGE).load()
 
-open('/tmp/cover.jpg', 'w+').write(cover.data)
-open('/tmp/cover.html', 'w+').write('<img src="%s">' % cover.data_uri)
+open('/tmp/cover.jpg', 'wb').write(cover.data)
+open('/tmp/cover.html', 'w').write('<img src="%s">' % cover.data_uri)

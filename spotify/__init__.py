5d6343004a08e0fb116b2f244dd74ea302af3f34
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-04-24 00:27:35 +0200

    Bump version number to 2.0.0b2

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import threading
 
 
-__version__ = '2.0.0b1'
+__version__ = '2.0.0b2'
 
 
 # Global reentrant lock to be held whenever libspotify functions are called or

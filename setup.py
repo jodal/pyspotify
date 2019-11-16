9e15a25b732a5fa52fbbccdba08dee7d185e7b3a
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-11-16 16:48:44 +0100

    setup: Move project setup to setup.cfg

--- setup.py
+++ setup.py
@@ -1,49 +1,6 @@
-from __future__ import unicode_literals
-
-import re
-
-
-from setuptools import find_packages, setup
-
-
-def read_file(filename):
-    with open(filename) as fh:
-        return fh.read()
-
-
-def get_version(filename):
-    init_py = read_file(filename)
-    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
-    return metadata['version']
-
+from setuptools import setup
 
 setup(
-    name='pyspotify',
-    version=get_version('spotify/__init__.py'),
-    url='http://pyspotify.mopidy.com/',
-    license='Apache License, Version 2.0',
-    author='Stein Magnus Jodal',
-    author_email='stein.magnus@jodal.no',
-    description='Python wrapper for libspotify',
-    long_description=read_file('README.rst'),
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    zip_safe=False,
-    include_package_data=True,
-    setup_requires=['cffi >= 1.0.0'],
-    cffi_modules=['spotify/_spotify_build.py:ffi'],
-    install_requires=['cffi >= 1.0.0'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries',
-    ],
+    # XXX Setting cffi_modules in setup.cfg does not have any effect.
+    cffi_modules=['spotify/_spotify_build.py:ffi']
 )
